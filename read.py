9853ab7ff078a8cebf2aebfe70e14706033e25ed
Mark Wu <q71038488@gmail.com>
2023-04-26 13:17:05 +0800

    data<100

--- read.py
+++ read.py
@@ -1,7 +1,7 @@
-sum = 0
+data = []
 count = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
-		sum += (len(line))
-		count +=1
-print(sum / count)
+		if len(line) < 100:
+			data.append(line)
+print('一共有', len(data), '長度小於100的留言')
