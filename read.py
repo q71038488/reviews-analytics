41df5ca034d2652ec31426bdc4fa735e3874954d
Mark Wu <q71038488@gmail.com>
2023-04-26 13:24:23 +0800

    search good

--- read.py
+++ read.py
@@ -1,7 +1,6 @@
 data = []
-count = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
-		if len(line) < 100:
+		if 'good' in line:
 			data.append(line)
-print('一共有', len(data), '長度小於100的留言')
+print(len(data))
